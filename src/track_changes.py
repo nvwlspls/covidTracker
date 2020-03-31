a01bcf2dce8f224ec1b9fd5b1e8488b57de1187b
Wayne Jessen <mremeat@gmail.com>
2020-03-30 19:59:35 -0700

    added header check, removed loop

--- src/track_changes.py
+++ src/track_changes.py
@@ -3,34 +3,53 @@
 import requests
 import time
 import os
+import json
+from json.decoder import JSONDecodeError
 from bs4 import BeautifulSoup
 from twilio.rest import Client
 from access.access import SPI, AUTH_TOKEN, PHONE_NUMBER
 
-COVID_NUM = 0
-
+# access infor
 account_sid = SPI
 auth_token = AUTH_TOKEN
+
+# create client
 client = Client(account_sid, auth_token)
-print(os.getcwd())
-while True:
 
-    site = requests.get('https://www.sandiegocounty.gov/content/sdc/hhsa/programs/phs/community_epidemiology/dc/2019-nCoV/status.html')
-    soup = BeautifulSoup(site.content, 'html.parser')
-    this_num = int(soup.select(".table > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2) > b:nth-child(1)")[0].text)
+# requests
+target_url = 'https://www.sandiegocounty.gov/content/sdc/hhsa/programs/phs/community_epidemiology/dc/2019-nCoV/status.html'
 
-    if this_num != COVID_NUM:
-        
-        diff = this_num -  COVID_NUM
-        COVID_NUM = this_num
+# get the data from the last change
+change_data = open(os.path.join(os.getcwd(), 'data', 'change_info.json'), 'w+')
 
-        message = "San Diego has reported {} new Coronvavirus cases today.  The total is now {}".format(diff, this_num)
+try:
+    change_data = json.load(change_data)
+except JSONDecodeError:
+    change_data = {}
+    change_data = requests.head(target_url)['ETag']
 
-        message = client.messages \
-                .create(
-                     body=message,
-                     from_='+12028901613',
-                     to=PHONE_NUMBER
-                 )
-    
-    time.sleep(60)
+
+
+site = requests.get()
+
+soup = BeautifulSoup(site.content, 'html.parser')
+case_num = int(soup.select(".table > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2) > b:nth-child(1)")[0].text)
+
+# data dir
+data_dir = os.path.join(os.getcwd(), 'data')
+
+stats_data = open(os.path.join(data_dir, 'stats'), 'w+')
+
+# current numbers
+
+diff = case_num -  COVID_NUM
+COVID_NUM = case_num
+
+text = "San Diego has reported {} new Coronvavirus cases today.  The total is now {}".format(diff, this_num)
+
+message = client.messages \
+        .create(
+                body=text,
+                from_='+12028901613',
+                to=PHONE_NUMBER
+            )
