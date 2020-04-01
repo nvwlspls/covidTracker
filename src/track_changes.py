47ec7688645421a3e3a0056a553997fe8e13c2b0
Wayne Jessen <wjessen@traconpharma.com>
2020-03-31 22:18:44 -0700

    updates to track_changes.py

--- src/track_changes.py
+++ src/track_changes.py
@@ -17,21 +17,43 @@ client = Client(account_sid, auth_token)
 # requests
 target_url = 'https://www.sandiegocounty.gov/content/sdc/hhsa/programs/phs/community_epidemiology/dc/2019-nCoV/status.html'
 
+
 # get the data from the last change
-change_data = open(os.path.join(os.getcwd(), 'data', 'change_info.json'), 'w+')
+change_data_file = open(os.path.join(os.getcwd(), 'data', 'change_info.json'), 'w+')
+
+import pdb; pdb.set_trace();
 
 try:
-    change_data = json.load(change_data)
+    change_data = json.load(change_data_file)
 except JSONDecodeError:
     change_data = {}
-    change_data = requests.head(target_url)['ETag']
+    # get the etag value and remove the quotes
+    change_data['etag'] = requests.head(target_url).headers['ETag'].strip('\"')
+    json.dump(change_data, change_data_file)
+    change_data_file.close()
 
+# if the etag is the same then we can stop
 
+curr_etag = requests.head(target_url).headers['ETag'].strip('\"')
 
-site = requests.get()
+if curr_etag != change_data:
+    # 
+    site = requests.get(target_url)
+    soup = BeautifulSoup(site.content, 'html.parser')
+    case_num = int(soup.select(".table > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2) > b:nth-child(1)")[0].text)
+    death_num = int(soup.select(".table > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(21) > td:nth-child(2)").text)
 
-soup = BeautifulSoup(site.content, 'html.parser')
-case_num = int(soup.select(".table > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2) > b:nth-child(1)")[0].text)
+    #open this file
+    case_data_file = open(os.path.join(os.getcwd()), 'data', 'case_data.json', \
+    'w+' )
+
+    try:
+        case_data = json.load(case_data_file)
+    except JSONDecodeError:
+        case_data = {}
+        case_data['total_cases'] = case_num
+        
+
 
 # data dir
 data_dir = os.path.join(os.getcwd(), 'data')
